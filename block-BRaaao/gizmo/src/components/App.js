20bb8042611816b355b62c230ef9ed778506f44a
KASHISH <kashu80525@gmail.com>
2024-01-27 16:12:16 +0530

    adding componrnts

--- block-BRaaao/gizmo/src/components/App.js
+++ block-BRaaao/gizmo/src/components/App.js
@@ -1,5 +1,7 @@
-import data from "../data.json";
-
+import Header from "./Header";
+import Hero from "./Hero";
+import Articles from "./Articles";
+import Footer from "./Footer";
 function App() {
   return (
     <>
@@ -7,62 +9,6 @@ function App() {
       <Hero />
       <Articles />
       <Footer />
-    </>
-  );
-}
-
-function Header() {
-  return (
-    <>
-      <div className="header">
-        <div className="logo">GIZMO</div>
-        <ul>
-          <li>HOME</li>
-          <li>ABOUT</li>
-          <li>CONTACT</li>
-        </ul>
-      </div>
-    </>
-  );
-}
-
-function Hero() {
-  return <h1>CURRENT NEWS OF THE DAY!</h1>;
-}
-
-function Articles() {
-  return (
-    <>
-      <h2>Articles</h2>
-      <div className="article-container">
-        {data.map((article) => (
-          <Article {...article} />
-        ))}
-      </div>
-    </>
-  );
-}
-
-function Article(props) {
-  return (
-    <div className="wrapper">
-      <img src={props.urlToImage} alt={props.title} width="200" />
-      <h3>{props.author}</h3>
-      <p>{props.title}</p>
-    </div>
-  );
-}
-function Footer() {
-  return (
-    <>
-      <div className="footer">
-        <div className="">2020 All Rights Reserved</div>
-        <ul className="footer-li">
-          <li>F</li>
-          <li>T</li>
-          <li>I</li>
-        </ul>
-      </div>
     </>
   );
 }
---
+++ block-BRaaao/gizmo/src/components/Header.js
@@ -0,0 +1,16 @@
+function Header() {
+  return (
+    <>
+      <div className="header">
+        <div className="logo">GIZMO</div>
+        <ul>
+          <li>HOME</li>
+          <li>ABOUT</li>
+          <li>CONTACT</li>
+        </ul>
+      </div>
+    </>
+  );
+}
+
+export default Header;

3b984d14b98b34656d1abc441096c31dbc6a1300
KASHISH <kashu80525@gmail.com>
2024-01-30 13:51:19 +0530

    adding button design app

---
+++ block-BRaaax/button/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Button from "./Button";
+
+function App() {
+  return (
+    <>
+      <Button label="Click Me!" />
+      <Button label="Click Me!" type="secondary" size="large" />
+      <Button label="Click Me!" type="tertiary" size="medium" />
+      <Button label="Click Me!" />
+    </>
+  );
+}
+
+export default App;
